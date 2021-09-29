3b319a496922abe322db6ec4f69008af97fcd572
sofiiasemchyshyn <89664363+sofiiasemchyshyn@users.noreply.github.com>
2021-09-29 10:56:10 +0300

    Add files via upload

---
+++ Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace IT1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader("C:\\Users\\User\\Desktop\\IT1\\Task1\\Input.txt"))
+                {
+
+                    String text = sr.ReadToEnd();
+                    Char[] symbols = text.ToCharArray();
+
+                    using (StreamWriter sw = new StreamWriter("C:\\Users\\User\\Desktop\\IT1\\Task1\\Output.txt", false))
+                    {
+
+                        for (int i = symbols.Length - 1; i >= 0; i--)
+                        {
+                            sw.Write(symbols[i]);
+                        }
+                    }
+                }
+                
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //тільки для додатніх чисел
+            Console.WriteLine("Введiть порядковий номер:");
+            int number = int.Parse(Console.ReadLine());
+            int[] F = new int[number];
+            F[0] = 0; F[1] = 1;
+            
+            for (int i = 2; i < number; i++)
+            {
+                F[i] = F[i - 1] + F[i - 2];
+            }
+
+            Console.WriteLine( number + " число Фiбоначчi рiвне " + F[number-1]);
+        }
+    }
+}
